d10fad7adc34f00123168aae8ac183e773b0ae09
«Denis <6545158@gmail.com>
2023-06-06 19:49:16 +0200

    2

--- src/main/java/lesson1/MainClassForLessonOne.java
+++ src/main/java/lesson1/MainClassForLessonOne.java
@@ -1,10 +1,14 @@
 package lesson1;
-
+import java.util.Scanner;
 public class MainClassForLessonOne {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println(s);
 
 
         }
-    }
-
 }
+
+
