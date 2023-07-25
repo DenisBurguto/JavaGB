a751d4bd6b353e4be6209bb3305ec5bee4394ac2
«Denis <6545158@gmail.com>
2023-07-25 13:50:43 +0300

    Exception HT2

--- src/main/java/ExceptionHT2/Task3.java
+++ src/main/java/ExceptionHT2/Task3.java
@@ -23,8 +23,7 @@ public class Task3 {
         }
     }
 
-    public static void printSum(Integer a, Integer b) {
-
+    public static void printSum(Integer a, Integer b) throws NullPointerException {
         System.out.println(a + b);
     }
 
